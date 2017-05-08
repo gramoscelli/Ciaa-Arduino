653551e0de0f7fc421711655d1f404d7bf3d779b
root <root@DESKTOP-6UJEURK.bvconline.com.ar>
2017-05-08 20:12:30 -0300

    New version

---
+++ cores/LPC4337/Arduino.h
@@ -0,0 +1,15 @@
+#ifndef _ARDUINO_H_
+#define _ARDUINO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+	void setup();
+	void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef _ARDUINO_H_ */
